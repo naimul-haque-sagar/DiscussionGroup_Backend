3aeb6c1f5c61aa724d116ca390000a5fbf6f19be
naimul <naimulhaquesagar24@gmail.com>
2020-06-02 17:33:17 +0600

    initial commit

---
+++ src/main/java/discussion/DiscussionGroupApplication.java
@@ -0,0 +1,13 @@
+package discussion;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DiscussionGroupApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DiscussionGroupApplication.class, args);
+	}
+
+}
---
+++ src/test/java/discussion/DiscussionGroupApplicationTests.java
@@ -0,0 +1,13 @@
+package discussion;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DiscussionGroupApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
