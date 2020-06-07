e48c35eb343ff64f82706e43703f6c36891178bc
naimul <naimulhaquesagar24@gmail.com>
2020-06-07 12:48:44 +0600

    jwt Authentication filter added

---
+++ src/main/java/discussion/security/JwtAuthenticationFilter.java
@@ -0,0 +1,52 @@
+package discussion.security;
+
+import java.io.IOException;
+
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
+import org.springframework.stereotype.Component;
+import org.springframework.util.StringUtils;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+@Component
+public class JwtAuthenticationFilter extends OncePerRequestFilter{
+	@Autowired
+	private JwtProvider jwtProvider;
+	@Autowired
+	private UserDetailsService userDetailsService;
+	
+	@Override
+	protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
+			throws ServletException, IOException {
+		String extractedJwt=getJwtFromHttpServletRequest(request);
+		System.out.println("result "+extractedJwt);
+		
+		if(StringUtils.hasText(extractedJwt)&& jwtProvider.validateExtractedJwtKey(extractedJwt)) {
+			String username=jwtProvider.getUsernameFromExtractedJwtToken(extractedJwt);
+			UserDetails principal=userDetailsService.loadUserByUsername(username);
+			UsernamePasswordAuthenticationToken authentication=
+					new UsernamePasswordAuthenticationToken(principal, null, principal.getAuthorities());
+			authentication.setDetails(new WebAuthenticationDetailsSource().buildDetails(request));
+			SecurityContextHolder.getContext().setAuthentication(authentication);
+		}
+		filterChain.doFilter(request, response);
+	}
+
+	private String getJwtFromHttpServletRequest(HttpServletRequest request) {
+		String bearerToken=request.getHeader("Authorization");
+		if(StringUtils .hasText(bearerToken) && bearerToken.startsWith("Bearer ")) {
+			return bearerToken.substring(7);
+		}
+		return bearerToken;
+	}
+
+}
--- src/main/java/discussion/security/JwtProvider.java
+++ src/main/java/discussion/security/JwtProvider.java
@@ -6,6 +6,7 @@ import java.security.KeyStore;
 import java.security.KeyStoreException;
 import java.security.NoSuchAlgorithmException;
 import java.security.PrivateKey;
+import java.security.PublicKey;
 import java.security.UnrecoverableKeyException;
 import java.security.cert.CertificateException;
 
@@ -16,6 +17,7 @@ import org.springframework.security.core.userdetails.User;
 import org.springframework.stereotype.Service;
 
 import discussion.exceptions.AppExceptionMessage;
+import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
 
 @Service
@@ -36,16 +38,37 @@ public class JwtProvider {
 	public String generateJwtToken(Authentication authentication) {
 		User principal=(User) authentication.getPrincipal();
 		return Jwts.builder().setSubject(principal.getUsername())
-				.signWith(privateKey()).compact();
+				.signWith(getPrivateKeyFromKeystore()).compact();
+	}
+	
+	public boolean validateExtractedJwtKey(String key) {
+		Jwts.parser().setSigningKey(getPublicKeyFromKeyStore()).parseClaimsJws(key);
+		return true;
 	}
 
-	private PrivateKey privateKey() {
+	private PublicKey getPublicKeyFromKeyStore() {
+		try {
+			return keyStore.getCertificate("discussion").getPublicKey();
+		} catch (KeyStoreException e) {
+			throw new AppExceptionMessage("Error occured while retriving public key");
+		}
+	}
+
+	private PrivateKey getPrivateKeyFromKeystore() {
 		try {
 			return (PrivateKey) keyStore.getKey("discussion", "discussion".toCharArray());
 		}catch(KeyStoreException|NoSuchAlgorithmException|UnrecoverableKeyException e) {
 			throw new AppExceptionMessage("Error occured while retriving private key");
 		}
 	}
+
+	public String getUsernameFromExtractedJwtToken(String extractedJwt) {
+		Claims claims=Jwts.parser().setSigningKey(getPublicKeyFromKeyStore())
+				.parseClaimsJws(extractedJwt).getBody();
+		return claims.getSubject();
+	}
+	
+	
 	
 	
 
