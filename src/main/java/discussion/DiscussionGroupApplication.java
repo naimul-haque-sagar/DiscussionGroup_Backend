635e719c94531af0bb3135cd654160f4502e1393
naimul-haque-sagar <naimulhaquesagar@gmail.com>
2020-07-13 22:46:05 +0600

    swagger added

--- src/main/java/discussion/DiscussionGroupApplication.java
+++ src/main/java/discussion/DiscussionGroupApplication.java
@@ -1,11 +1,14 @@
 package discussion;
 
+import discussion.configuration.SwaggerConfiguration;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Import;
 import org.springframework.scheduling.annotation.EnableAsync;
 
 @SpringBootApplication
 @EnableAsync
+@Import(SwaggerConfiguration.class)
 public class DiscussionGroupApplication {
 
 	public static void main(String[] args) {
--- src/main/java/discussion/configuration/SecurityConfiguration.java
+++ src/main/java/discussion/configuration/SecurityConfiguration.java
@@ -37,6 +37,13 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter{
 		.authorizeRequests()
 		.antMatchers("/api/auth/**")
 		.permitAll()
+				.antMatchers("/v2/api-docs",
+						"/configuration/ui",
+						"/swagger-resources/**",
+						"/configuration/security",
+						"/swagger-ui.html",
+						"/webjars/**")
+				.permitAll()
 		.anyRequest()
 		.authenticated()
 		.and().sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
